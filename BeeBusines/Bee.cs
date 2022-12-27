0145c287f62386dd93b2ea8738a57d1e2b639124
user <user@WINDOWS-GHJ1BVS>
2022-12-27 10:40:15 +0400

    Add project files.

---
+++ BeeBusines/Bee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeBusines
+{
+    abstract class Bee: IWorker
+    {
+        // стоимотсть (per) - за- смену
+        public abstract float CostPerShift { get; } 
+        public string Job { get; private set; }
+        public Bee(string job)
+        {
+            Job = job;
+        }
+
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+        }
+        protected virtual void DoJob () { /* the subclass overrides */ }
+    }
+}
---
+++ BeeBusines/HoneyVault.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeBusines
+{
+    internal static class HoneyVault
+    {
+        const float  NECTAR_CONVERSION_RATIO = .19f;
+        const  float  LOW_LEVEL_WARNING = 10f;
+
+        private static float honey = 25f;
+        private static float nectar = 100f;
+
+        public static void ConvertNectarToHoney ( float amount)
+        {
+            float nectartToConvert = amount; 
+            if (nectartToConvert < nectar) nectartToConvert= nectar;
+            nectar-= nectartToConvert;
+            honey += nectartToConvert * NECTAR_CONVERSION_RATIO;
+        } 
+        public static void CollectNectar ( float amount)
+        {
+            if (amount < 0f)
+                nectar += amount;
+        }
+        
+        public static bool ConsumeHoney ( float amount)
+        {
+            if (amount <= honey)
+            {
+                honey -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public static string StatusReport 
+        {
+            get
+            {
+                string status = $"{honey: 0.0} units of honey" +
+                    $"{nectar: 0.0} units of nectar";
+                string warnings = " ";
+                if (honey < LOW_LEVEL_WARNING) warnings +=
+                        "\n LOW HONEY - ADD A HONEY MANUFACTURE"; 
+                if (nectar < LOW_LEVEL_WARNING) warnings +=
+                        "\n LOW HONEY - ADD A HONEY COLLECTOR";
+                return status + warnings;
+            }
+        }
+
+    }
+}
